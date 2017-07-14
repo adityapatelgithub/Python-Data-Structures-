5c942c414aa6110b0743dc040c6cde07fdb296a0
adityapatelgithub <adityapatelgithub>
2017-07-13 21:20:09 -0400

    Stack modified

--- Stack.py
+++ Stack.py
@@ -2,8 +2,9 @@
 Out (LIFO) implementation. ArrayStack uses Python List to implement stack.
 LinkedListStack uses Linked Lists to implement stack"""
 
+
 class ArrayStack:
-    """Stack implementation using Python Lists"""
+    """Stack implementation using Python Lists. Initialize with no parameters"""
 
     def __init__(self):
         """Create empty stack."""
@@ -14,7 +15,7 @@ class ArrayStack:
         return len(self._data)
 
     def is_empty(self):
-        """Return True is the stack is empty."""
+        """Return True if the stack is empty."""
         return len(self._data)==0
 
     def push(self, element):
@@ -33,4 +34,49 @@ class ArrayStack:
             raise Empty('Stack is empty')
         return self._data.pop()         # array.pop()removes and returns the last element 
     
+class LinkedListStack():
+    """Stack implementation using Linked List. Initialize with no parameters"""
+    class Node:
+        def __init__(self, value, next):
+            self._value = value
+            self._next = next
+    def __init__(self):
+        """Create empty stack"""
+        self._head = None
+        self._size = 0
 
+    def __len__(self):
+        """Return the sumer of elements in the Stack"""
+        return self._size
+
+    def is_empty(self):
+        """Return True if the stack is empty"""
+        return self._size==0
+
+    def push(self, element):
+        """Add element to the top of stack"""
+        self._head = LinkedListStack.Node( element, self._head )
+        self._size += 1 
+
+    def top(self):
+        """Return the top element"""
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._head._value
+
+    def pop(self):
+        """Remove and return the top element. If empty, raise Empty exception"""
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        result = self._head._value
+        self._head = self._head._next
+        self._size -= 1
+        return result
+    
+
+
+
+
+
+        
+    
